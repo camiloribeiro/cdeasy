b022ee00c00e1236e9e98c6a447411705e4a4aad
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-08-22 22:45:07 +0200

    adding show changelog

--- dsl/pipeline/pipeline.groovy
+++ dsl/pipeline/pipeline.groovy
@@ -1,12 +1,12 @@
 deliveryPipelineView('Dummy Pipeline Example') {
   pipelineInstances(5)
     showAggregatedPipeline()
-    columns(2)
+    columns(1)
     sorting(Sorting.TITLE)
     updateInterval(60)
     enableManualTriggers(true)
     showAvatars()
-    showChangeLog()
+    showChangeLog(true)
     pipelines {
       component('Dummy Pipeline Example', 'unit_test')
     }
