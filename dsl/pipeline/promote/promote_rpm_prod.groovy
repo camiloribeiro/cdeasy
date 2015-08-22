e884620bd3793be960c25486eb8821427dcef654
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-08-23 01:19:59 +0200

    Replacing parametrized build to simple deploy

--- dsl/pipeline/promote/promote_rpm_prod.groovy
+++ dsl/pipeline/promote/promote_rpm_prod.groovy
@@ -11,11 +11,7 @@ job("promote_rpm_to_production") {
   }
 
   publishers {
-    downstreamParameterized {
-      trigger("deploy_to_beta", 'SUCCESS', true, null) {
-        currentBuild()
-      }
-    }
+    buildPipelineTrigger('deploy_to_beta, deploy_to_production')
   }
 
 }
