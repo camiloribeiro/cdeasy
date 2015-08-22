4db9f88d4fb23929d7e3538c62bfc898cf01e069
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-08-23 00:01:56 +0200

    removing aggregated view

--- dsl/pipeline/pipeline.groovy
+++ dsl/pipeline/pipeline.groovy
@@ -1,9 +1,8 @@
 deliveryPipelineView('Dummy Pipeline Example') {
   pipelineInstances(5)
-    showAggregatedPipeline(false)
     columns(1)
     sorting(Sorting.TITLE)
-    updateInterval(60)
+    updateInterval(10)
     enableManualTriggers(true)
     showAvatars()
     showChangeLog(true)
