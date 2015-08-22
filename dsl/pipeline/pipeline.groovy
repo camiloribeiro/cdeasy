2039acf882433071156e864d06be9b0f6a865e93
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-08-22 22:13:39 +0200

    Adding new title to the pipeline

--- dsl/pipeline/pipeline.groovy
+++ dsl/pipeline/pipeline.groovy
@@ -1,10 +1,10 @@
-deliveryPipelineView('dummy_pipeline_example') {
+deliveryPipelineView('Dummy Pipeline Example') {
   pipelineInstances(5)
     showAggregatedPipeline()
     columns(2)
     sorting(Sorting.TITLE)
     updateInterval(60)
-    enableManualTriggers()
+    enableManualTriggers(true)
     showAvatars()
     showChangeLog()
     pipelines {
