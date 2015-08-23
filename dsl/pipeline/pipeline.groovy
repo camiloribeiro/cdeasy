a7d290916df283a630c4ff07101ffe3f59b0ae7c
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-08-23 05:44:00 +0200

    Improving pipeline

--- dsl/pipeline/pipeline.groovy
+++ dsl/pipeline/pipeline.groovy
@@ -1,11 +1,11 @@
 deliveryPipelineView('Dummy Pipeline Example') {
   pipelineInstances(5)
     columns(1)
-    sorting(Sorting.TITLE)
+    sorting(Sorting.LAST_ACTIVITY)
     updateInterval(10)
     enableManualTriggers(true)
     showAvatars()
-    showChangeLog(true)
+    showChangeLog()
     pipelines {
       component('Dummy Pipeline Example', 'unit_test')
     }
