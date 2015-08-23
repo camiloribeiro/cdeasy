4767d8f879cd4a5fd500b4a3b496ca311ee12134
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-08-23 06:21:40 +0200

    setting boot2docker-vm machine back

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@ docker-compose stop
 docker-compose rm -v --force
 
 if [ "$(uname)" == "Darwin" ]; then
-  VBoxManage controlvm docker natpf1 delete "jenkins" &>/dev/null
-  VBoxManage controlvm docker natpf1 delete "agentserver" &>/dev/null
-  VBoxManage controlvm docker natpf1 "jenkins,tcp,127.0.0.1,8080,,8080" &>/dev/null
-  VBoxManage controlvm docker natpf1 "agentserver,tcp,127.0.0.1,4243,,4243" &>/dev/null
+  VBoxManage controlvm boot2docker-vm natpf1 delete "jenkins" &>/dev/null
+  VBoxManage controlvm boot2docker-vm natpf1 delete "agentserver" &>/dev/null
+  VBoxManage controlvm boot2docker-vm natpf1 "jenkins,tcp,127.0.0.1,8080,,8080" &>/dev/null
+  VBoxManage controlvm boot2docker-vm natpf1 "agentserver,tcp,127.0.0.1,4243,,4243" &>/dev/null
 fi
 
 docker-compose build jenkins 
