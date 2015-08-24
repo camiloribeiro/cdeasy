97d51cc87f50553233cd79c262c4c2c00d9e2ec0
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-08-24 21:11:13 +0200

    Adding example that runs a docker compose cluster inside a docker slave = dockerception

---
+++ dsl/pipeline/agnostic/running_compose.groovy
@@ -0,0 +1,16 @@
+job("running_compose_with_docker") {
+  scm {
+    git {
+      remote {
+        name('origin')
+        url ("https://github.com/b00giZm/docker-compose-nodejs-examples.git")
+      }
+    }
+  }
+  steps {
+    shell 'cd 02-express-redis-nodemon'
+    shell 'docker-compose up -d'
+    shell 'curl http://localhost:3030'
+  }
+}
+
