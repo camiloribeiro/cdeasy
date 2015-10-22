41f50ecea4820208af175c40f803ea0856453c18
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-10-22 18:03:26 +0200

    Simplifying startup

--- docker/jenkins/start.sh
+++ docker/jenkins/start.sh
@@ -1,6 +1,3 @@
 sleep 3
-service docker start
-sleep 3
-nohup /usr/bin/docker -d --selinux-enabled -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock  > /dev/null 2>&1 &
-sleep 3
+service docker start 
 /usr/local/bin/jenkins.sh
