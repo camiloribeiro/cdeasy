5a381514adc01ee573400aedac534db5dbfbdac0
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-11-02 15:22:14 +0100

    removing no needed dockerfiles

--- docker/agentserver/start.sh
+++
@@ -1,6 +0,0 @@
-/usr/sbin/sshd
-service docker start
-nohup /usr/bin/docker -d  --selinux-enabled -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock  > /dev/null 2>&1 &
-sleep 3
-docker pull camiloribeiro/dockerslave
-tail -f /var/log/docker.log
--- docker/dockerslave/start.sh
+++
@@ -1,6 +0,0 @@
-curl -sSL https://get.docker.com/ | sh
-curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-service docker start
-/usr/bin/docker -d  --selinux-enabled -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock
