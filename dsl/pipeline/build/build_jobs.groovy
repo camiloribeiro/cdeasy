6d5ef5c836c944ef7505f36e5711e6ac4b94b928
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-11-02 10:23:00 +0100

    Replacing standard job dsl by groovy example

--- dsl/pipeline/build/build_jobs.groovy
+++ dsl/pipeline/build/build_jobs.groovy
@@ -1,5 +1,16 @@
 package pipeline.build
 
-/**
- * Created by camilo.ribeiro on 02/11/15.
- */
+import com.camiloribeiro.cdeasy.job.JobHelper
+import javaposse.jobdsl.dsl.DslFactory
+import javaposse.jobdsl.dsl.Job
+import static com.camiloribeiro.cdeasy.support.Support.BuildConditions.SUCCESS
+
+Job code_analisys = JobHelper.createJob(this as DslFactory, "code_analysis")
+JobHelper.addStep(code_analisys, "sleep \$((RANDOM%10+5))")
+JobHelper.addDownstreamParameterized(code_analisys, ["build_rpm"], SUCCESS)
+JobHelper.addDeliveryPipelineConfiguration(code_analisys, 'Build', 'Code Analysis')
+
+Job build_rpm = JobHelper.createJob(this as DslFactory, "build_rpm")
+JobHelper.addStep(build_rpm, "sleep \$((RANDOM%10+5))")
+JobHelper.addDownstreamParameterized(build_rpm, ["promote_rpm_to_dev"], SUCCESS)
+JobHelper.addDeliveryPipelineConfiguration(build_rpm, 'Build', 'Build RPM')
