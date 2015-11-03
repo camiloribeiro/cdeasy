b2c13a16a6d2719819532bc2dd9fa5abd8bf5236
Camilo Ribeiro <camilo@camiloribeiro.com>
2015-11-03 10:32:32 +0100

    Fixing typo

--- dsl/pipeline/jobs/promote_jobs.groovy
+++ dsl/pipeline/jobs/promote_jobs.groovy
@@ -4,7 +4,7 @@ import com.camiloribeiro.cdeasy.job.JobHelper
 import javaposse.jobdsl.dsl.DslFactory
 import javaposse.jobdsl.dsl.Job
 
-Job promote_rpm_to_dev = JobHelper.createJob(this as DslFactory, "promote_rpm_to_stage")
+Job promote_rpm_to_dev = JobHelper.createJob(this as DslFactory, "promote_rpm_to_dev")
 JobHelper.addStep(promote_rpm_to_dev, "sleep \$((RANDOM%10+5))")
 JobHelper.addDeliveryPipelineConfiguration(promote_rpm_to_dev, 'Build', 'Promote RPM to dev')
 JobHelper.addDownstreamParameterized(promote_rpm_to_dev, ["service_level_test_1","service_level_test_2","pen_test","stress_test","load_test"], "SUCCESS")
