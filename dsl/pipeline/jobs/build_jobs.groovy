535fe382aea03ba9389c3e9e6eb3c5a75b0304d4
Gianpaolo Macario <gmacario@gmail.com>
2016-01-16 07:55:31 +0100

    Fix variable spelling

--- dsl/pipeline/jobs/build_jobs.groovy
+++ dsl/pipeline/jobs/build_jobs.groovy
@@ -4,10 +4,10 @@ import com.camiloribeiro.cdeasy.job.JobHelper
 import javaposse.jobdsl.dsl.DslFactory
 import javaposse.jobdsl.dsl.Job
 
-Job code_analisys = JobHelper.createJob(this as DslFactory, "code_analysis")
-JobHelper.addStep(code_analisys, "sleep \$((RANDOM%10+5))")
-JobHelper.addDownstreamParameterized(code_analisys, ["build_rpm"], "SUCCESS")
-JobHelper.addDeliveryPipelineConfiguration(code_analisys, 'Build', 'Code Analysis')
+Job code_analysis = JobHelper.createJob(this as DslFactory, "code_analysis")
+JobHelper.addStep(code_analysis, "sleep \$((RANDOM%10+5))")
+JobHelper.addDownstreamParameterized(ccode_analysis, ["build_rpm"], "SUCCESS")
+JobHelper.addDeliveryPipelineConfiguration(code_analysis, 'Build', 'Code Analysis')
 
 Job build_rpm = JobHelper.createJob(this as DslFactory, "build_rpm")
 JobHelper.addStep(build_rpm, "sleep \$((RANDOM%10+5))")
