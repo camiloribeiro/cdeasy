8e3bca27471727c22803e62368076dc32d486b19
Camilo Ribeiro <camilo.ribeiro@magine.com>
2016-05-02 17:22:23 +0200

    It is o longer needed to use git to get the changes :) just update the code and run the update job.

--- docker/jenkins/seed.groovy
+++ docker/jenkins/seed.groovy
@@ -1,26 +1,18 @@
 import jenkins.model.*;
 import hudson.model.FreeStyleProject;
-import hudson.plugins.git.GitSCM;
 import hudson.tasks.Shell;
 import javaposse.jobdsl.plugin.*;
 
-def url = "https://github.com/camiloribeiro/cdeasy.git"
-def jobName = "seed"
-
-project = Jenkins.instance.createProject(FreeStyleProject, jobName)
-def gitScm = new GitSCM(url)
-gitScm.branches = [new hudson.plugins.git.BranchSpec("*/master")]
-project.scm = gitScm
+project = Jenkins.instance.createProject(FreeStyleProject, "seed")
 
 project.getBuildersList().clear()
-
 project.getBuildersList().add(new Shell("docker run -v \$PWD:/groovy -w /groovy camiloribeiro/gradle:1.10 build"));
 
 project.getBuildersList().add(new ExecuteDslScripts(
   new ExecuteDslScripts.ScriptLocation("false","dsl/**/*.groovy",null),
   false,
-  RemovedJobAction.IGNORE,
-  RemovedViewAction.IGNORE,
+  RemovedJobAction.DELETE,
+  RemovedViewAction.DELETE,
   LookupStrategy.JENKINS_ROOT,
   "src/main/groovy")
 );
