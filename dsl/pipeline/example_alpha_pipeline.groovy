fada6f792243eeafd37bf1fa59868a40b2e26f74
Camilo Ribeiro <camilo.ribeiro@magine.com>
2016-05-02 17:23:22 +0200

    Adding job that runs the new pipeline for jenkins 2

---
+++ dsl/pipeline/example_alpha_pipeline.groovy
@@ -0,0 +1,15 @@
+workflowJob('example_pipeline_jenkins2') {
+  definition {
+    cpsScm {
+      scm {
+        git{
+          remote{
+            url("https://github.com/camiloribeiro/cdeasy.git")
+          }
+          branch("master")
+        }
+      }
+      scriptPath("Jenkinsfile")
+    }
+  }
+}
